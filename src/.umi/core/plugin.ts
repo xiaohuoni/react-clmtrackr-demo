4bd309ff4f7cc5933825c7e47b0e6890946d6c4e
xiaohuoni <448627663@qq.com>
2020-06-17 17:01:01 +0800

    first commit

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/xiaohuoni/Desktop/umi-watch-test/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
